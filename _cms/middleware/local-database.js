842ff77d09f4c07bfcd30660464d09eaa9a475da
Tim Spears <tspears1@gmail.com>
2024-12-08 19:41:07 -0500

    update LocalDB

--- _cms/middleware/local-database.js
+++ _cms/middleware/local-database.js
@@ -161,38 +161,6 @@ class LocalDB {
    }
 
    /**
-    * Read post data from file.
-    *
-    * @param {string} path
-    * @returns {Promise<object>} post
-    */
-   async readPostData(path) {
-      return await Deno.readTextFile(path).then(post => JSON.parse(post))
-   }
-
-   /**
-    * Break file relative path into `structure`, `postType`, and `slug`.
-    */
-   breakPath(path) {
-      const regex = /^\/(?<structure>[^\/]+)\/(?<postType>[^\/]+)\/(?<slug>[^\/]+)\.json$/
-      const matches = path.match(regex)
-      return matches ? matches.groups : null
-   }
-
-   /**
-    * Create new post data.
-    *
-    * @param {string} path
-    * @returns {Promise<object>} post
-    */
-   async createPostData(path) {
-      const { structure, postType, slug } = this.breakPath(path)
-      const id = this.createNewId()
-      const post = await this.readPostData(path)
-      return { id, structure, postType, slug, ...post }
-   }
-
-   /**
     * Query Map for post data by property.
     *
     * @param {string} property
@@ -219,30 +187,6 @@ class LocalDB {
                return entry
          }
       })
-   }
-
-   /**
-    * Query Map for post data that matches structure, postType, and slug.
-    *
-    * @param {string} path
-    * @returns {object} post
-    */
-   queryPost(path) {
-      const { structure, postType, slug } = this.breakPath(path)
-      const result = this.dataArray.find((entry) => {
-         const post = entry[1]
-         if (!Object.hasOwn(post, 'structure')
-            || !Object.hasOwn(post, 'postType')
-            || !Object.hasOwn(post, 'slug')) {
-               return
-         }
-         if (post.structure === structure
-            && post.postType === postType
-            && post.slug === slug) {
-               return entry
-         }
-      })
-      return result ? result[1] : null
    }
 }
 
